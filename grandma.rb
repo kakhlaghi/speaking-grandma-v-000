43d8493948d1341d40335274c8621ca17621d2ee
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-08-18 01:56:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   if phrase.upcase
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    return ""
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     return "# HUH?! SPEAK UP, SONNY!"
   end
