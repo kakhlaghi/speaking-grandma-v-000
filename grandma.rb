11d815f1e8776b33b656d750ced45a0d36c7e6e2
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-08-18 01:55:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == ""
+  elsif phrase == "I LOVE YOU GRANDMA!"
   else
     return "# HUH?! SPEAK UP, SONNY!"
   end
